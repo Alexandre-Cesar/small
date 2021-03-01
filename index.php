45dee0ac6176f261928b0e04c40db9f089bc2c40
Alexandre-Cesar <72271939+Alexandre-Cesar@users.noreply.github.com>
2021-03-01 15:37:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php  
 
 include 'header.php';
-include 'db.php';
+include 'conexao.php';
 #conteudo da pagina
 if(isset($_GET['pagina'])){
 	$pagina = $_GET['pagina'];
@@ -10,19 +10,14 @@ else $pagina = 'home';
 switch ($pagina) {
 	
 	case 'carros': include 'cursos.php';
-		break;
+	break;
 
-		case 'motos': include 'motos.php';
-		break;
+	case 'inserir_carro': include 'inserir_carro.php';
+	break;
 
-		case 'inserir_carro': include 'inserir_carro.php';
-		break;
-
-		case 'inserir_moto': include 'inserir_moto.php';
-		break;
-
-		default:  include 'home.php';
-		break;
+	default:  include 'home.php';
+	break;
 }
-	include 'footer.php';
+
+include 'footer.php';
 ?>
